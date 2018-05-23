2b7e001780bfb5d8b01ad428252150b57e64967e
glazkov722 <glazkov722@gmail.com>
2018-05-23 02:17:35 -0400

    edit php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>HTML5 : Hello World Example</title>
+    </head>
+    <body>
+        <h1>Hello World</h1>
+        <p>
+            Hey there! <br>
+          Привет!
+        </p>
+    </body>
+</html>
+
+<!DOCTYPE html>
+</head>
+    <body>
+        <h1>Инструктор Кекс</h1>
+        <p>В последующих курсах вам будет часто помогать с освоением тонкостей HTML и CSS инструктор Кексик. Дадим же ему возможность представиться:</p>
+        <img src="https://www.khabarovskadm.ru/districts/komitet-zhd-rayon/images/foto-rukovoditeley/Andreev.jpg" class="photocard">
+        <hr>
+        <blockquote>
+            <p>Привет! Меня зовут Кекс и я ваш будущий инструктор. Я веб-разработчик и живу в Санкт-Петербурге. Мои самые известные проекты:<br>
+            блог Cat Energy,<br>
+            курс про ссылки и изображения в HTML Academy,<br>
+            курс про HTML5 там же.</p>
+            <p>До встречи в последующих курсах!</p>
+        </blockquote>
+    </body>
+</html>
